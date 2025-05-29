7ddf652ae84f6f567fed2dd1ac08dcdb9c5f6a8a
Justin <j.hebenstreit25@gmail.com>
2025-05-28 19:35:01 -0500

    added new filters and fixed styling

--- src/components/CarInformation/CarList/CarFilters/Components/Dropdowns.tsx
+++ src/components/CarInformation/CarList/CarFilters/Components/Dropdowns.tsx
@@ -1,6 +1,6 @@
-import { DropdownsProps } from "@/components/CarInformation/CarList/CarFilters/interfaces/DropdownsProps";
+import { DropdownsProps } from '@/components/CarInformation/CarList/CarFilters/interfaces/DropdownsProps';
 
-const renderStars = (count: number) => "★".repeat(count);
+const renderStars = (count: number) => '★'.repeat(count);
 
 export default function Dropdowns({
   onStarsChange,
@@ -21,86 +21,118 @@ export default function Dropdowns({
 }: DropdownsProps) {
   return (
     <>
-        <label className="DropdownLabel starFilter">
-          Star Rank:
-          <select className="starRanks" value={selectedStars ?? "All"} onChange={onStarsChange}>
-            <option value="All">All Stars</option>
-            {availableStars.map(star => (
-              <option key={star} value={star}>
-                {renderStars(star)}
-              </option>
-            ))}
-          </select>
-        </label>
+      <label className="DropdownLabel starFilter">
+        Star Rank:
+        <select
+          className="starRanks"
+          value={selectedStars ?? 'All'}
+          onChange={onStarsChange}
+        >
+          <option value="All">All Stars</option>
+          {availableStars.map((star) => (
+            <option
+              key={star}
+              value={star}
+            >
+              {renderStars(star)}
+            </option>
+          ))}
+        </select>
+      </label>
 
-        <label className="DropdownLabel classFilter">
-          Car Class:
-          <select className="classSelect" value={selectedClass} onChange={onClassChange}>
-            <option value="All Classes">All Classes</option>
-            {["D", "C", "B", "A", "S"].map(cls => (
-              <option key={cls} value={cls}>
-                {cls}
-              </option>
-            ))}
-          </select>
-        </label>
+      <label className="DropdownLabel classFilter">
+        Class:
+        <select
+          className="classSelect"
+          value={selectedClass}
+          onChange={onClassChange}
+        >
+          <option value="All Classes">All Classes</option>
+          {['D', 'C', 'B', 'A', 'S'].map((cls) => (
+            <option
+              key={cls}
+              value={cls}
+            >
+              {cls}
+            </option>
+          ))}
+        </select>
+      </label>
 
-        <label className="DropdownLabel unitFilter">
-          Units:
-          <select value={unitPreference} onChange={onUnitChange}>
-            <option value="metric">Metric</option>
-            <option value="imperial">Imperial</option>
-          </select>
-        </label>
+      <label className="DropdownLabel unitFilter">
+        Units:
+        <select
+          value={unitPreference}
+          onChange={onUnitChange}
+        >
+          <option value="metric">Metric</option>
+          <option value="imperial">Imperial</option>
+        </select>
+      </label>
 
-        <label className="DropdownLabel rarityFilter">
-          Rarity:
-          <select value={selectedRarity || ""} onChange={e => onRarityChange(e.target.value || null)}>
-            <option value="">All Rarities</option>
-            {["Uncommon", "Rare", "Epic"].map(rarity => (
-              <option
-                key={rarity}
-                value={rarity}
-                className={
-                  rarity === "Uncommon"
-                    ? "optionUncommon"
-                    : rarity === "Rare"
-                    ? "optionRare"
-                    : rarity === "Epic"
-                    ? "optionEpic"
-                    : ""
-                }
-              >
-                {rarity}
-              </option>
-            ))}
-          </select>
-        </label>
+      <label className="DropdownLabel rarityFilter">
+        Rarity:
+        <select
+          value={selectedRarity || ''}
+          onChange={(e) => onRarityChange(e.target.value || null)}
+        >
+          <option value="">All Rarities</option>
+          {['Uncommon', 'Rare', 'Epic'].map((rarity) => (
+            <option
+              key={rarity}
+              value={rarity}
+              className={
+                rarity === 'Uncommon'
+                  ? 'optionUncommon'
+                  : rarity === 'Rare'
+                  ? 'optionRare'
+                  : rarity === 'Epic'
+                  ? 'optionEpic'
+                  : ''
+              }
+            >
+              {rarity}
+            </option>
+          ))}
+        </select>
+      </label>
 
-        <label className="DropdownLabel brandFilter">
-          Brand:
-          <select value={selectedBrand || ""} onChange={onBrandChange}>
-            <option value="">All Brands</option>
-            {availableBrands.map(brand => (
-              <option key={brand} value={brand}>
-                {brand}
-              </option>
-            ))}
-          </select>
-        </label>
+      <label className="DropdownLabel brandFilter">
+        Brand:
+        <select
+          className="scrollableDropdown"
+          value={selectedBrand || ''}
+          onChange={onBrandChange}
+        >
+          <option value="">All Brands</option>
+          {availableBrands.map((brand) => (
+            <option
+              key={brand}
+              value={brand}
+            >
+              {brand}
+            </option>
+          ))}
+        </select>
+      </label>
 
-        <label className="DropdownLabel countryFilter">
-          Country:
-          <select value={selectedCountry || ""} onChange={onCountryChange}>
-            <option value="">All Countries</option>
-            {availableCountries.map(country => (
-              <option key={country} value={country}>
-                {country}
-              </option>
-            ))}
-          </select>
-        </label>
-
+      <label className="DropdownLabel countryFilter">
+        Country:
+        <select
+          value={selectedCountry || ''}
+          onChange={onCountryChange}
+        >
+          <option value="">All Countries</option>
+          {availableCountries.map((country) => (
+            <option
+              key={country}
+              value={country}
+            >
+              {country}
+            </option>
+          ))}
+        </select>
+      </label>
     </>
   );
 }
