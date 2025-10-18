dcad826961c0c1839af8bd74c45a4791a6448562
Justin <j.hebenstreit25@gmail.com>
2025-10-18 13:56:15 -0500

    updated vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,9 +1,14 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_CARS_API_BASE_URL: string; // Define other variables here as needed
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
+  readonly VITE_CARS_API_BASE_URL: string;
+  readonly VITE_CONTENT_API_BASE_URL: string;
+  readonly VITE_COMMENTS_API_BASE_URL: string;
+  readonly VITE_AUTH_API_URL: string;
+  readonly VITE_IMG_CDN_BASE: string;
+  // add others here as you introduce them
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
