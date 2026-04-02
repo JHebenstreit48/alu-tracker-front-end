39840581407eded3b8334f37a5937927132994c4
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-04-02 17:16:34 -0500

    fixed some issues with stages tables

--- src/components/CarDetails/Tables/StarsStats/components/StageHeader.tsx
+++ src/components/CarDetails/Tables/StarsStats/components/StageHeader.tsx
@@ -2,16 +2,21 @@ import { getImageUrl } from "@/utils/shared/imageUrl";
 
 type Props = {
   stage: number;
+  starRank: number;
 };
 
 const STAR_ICON_REL_PATH = "/images/icons/star.png";
 
-export default function StageHeader({ stage }: Props) {
+export default function StageHeader({ stage, starRank }: Props) {
   const starIconUrl = getImageUrl(STAR_ICON_REL_PATH);
 
   return (
     <div className="stageHeader">
-      <img src={starIconUrl} alt="Star" className="starIcon stageHeaderStarIcon" />
+      <div className="starIconWrapper">
+        {Array.from({ length: starRank }).map((_, i) => (
+          <img key={i} src={starIconUrl} alt="Star" className="starIcon stageHeaderStarIcon" />
+        ))}
+      </div>
       <span className="stageLabel">Stage {String(stage).padStart(2, "0")}</span>
     </div>
   );
